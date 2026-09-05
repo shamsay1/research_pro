88a597680a686acd9ab9060d5c52170d603fe8df
shamisboy <shamsay70@gmail.com>
2026-09-05 10:34:47 +0300

    anothers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        $middleware->redirectGuestsTo('/login');
+
+        $middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
