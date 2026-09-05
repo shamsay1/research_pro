6ee18da674e9027119040bbf116fd41401954fe1
shamisboy <shamsay70@gmail.com>
2026-09-05 10:50:37 +0300

    last changestesting

--- config/services.php
+++ config/services.php
@@ -7,10 +7,8 @@ return [
     | Third Party Services
     |--------------------------------------------------------------------------
     |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
+    | Here you may configure the credentials for third party services
+    | such as Mailgun, Postmark, AWS, Resend and Slack.
     |
     */
 
